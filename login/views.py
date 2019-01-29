368d1089b11b9be123b46190d08b51aa89e899a0
hkimiaa <kimiahamdieh@gmail.com>
2019-01-29 22:21:45 +0330

    MODIFY register views

--- login/views.py
+++ login/views.py
@@ -9,38 +9,55 @@ def index(request):
 
 def my_account_job_seeker(request):
     login_form = LoginForm
-    register_form = JobSeekerRegisterForm
+    register_form = RegisterForm
     context = {'login_form': login_form, 'register_form': register_form}
     return render(request, 'my-account-job-seeker.html', context)
 
 
 def my_account_employer(request):
     login_form = LoginForm
-    register_form = EmployerRegisterForm
-    context = {'login_form': login_form, 'register_form': register_form}
+    register_form = RegisterForm
+    employer_register_form = EmployerRegisterForm
+    context = {'login_form': login_form, 'register_form': register_form,
+               'employer_register_form': employer_register_form}
     return render(request, 'my-account-employer.html', context)
 
 
 def job_seeker_register(request):
-    # if request.method == 'POST':
-    #     user_form = RegisterForm(request.POST)
-    #     profile_form = ProfileForm(request.POST, request.FILES)
-    #     if user_form.is_valid() and profile_form.is_valid():
-    #         user = user_form.save(commit=False)
-    #         user.set_password(user_form.cleaned_data['password'])
-    #         user.save()
-    #         user.profile.image = profile_form.cleaned_data['image']
-    #         login(request, user)
-    #         return redirect('home')
-    #     else:
-    #         return redirect('index')
-    # else:
-    #     return redirect('index')
-    pass
+    if request.method == 'POST':
+        user_form = RegisterForm(request.POST)
+        # profile_form = JobSeekerRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user = user_form.save(commit=False)
+            user.set_password(user_form.cleaned_data['password'])
+            user.save()
+            # user.jobseekerprofile.bio = profile_form.cleaned_data['bio']
+            # user.jobseekerprofile.cv = profile_form.cleaned_data['cv']
+            login(request, user)
+            return redirect('home')  # TODO
+        else:
+            return redirect('index')  # TODO
+    else:
+        return redirect('index')  # TODO
 
 
 def employer_register(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        user_form = RegisterForm(request.POST)
+        profile_form = EmployerRegisterForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save(commit=False)
+            user.set_password(user_form.cleaned_data['password'])
+            user.save()
+            user.employerprofile.company_name = profile_form.cleaned_data['company_name']
+            user.employerprofile.company_type = profile_form.cleaned_data['company_type']
+            user.employerprofile.company_disc = profile_form.cleaned_data['company_disc']
+            login(request, user)
+            return redirect('home')  # TODO
+        else:
+            return redirect('index')  # TODO
+    else:
+        return redirect('index')  # TODO
 
 
 def job_seeker_login(request):

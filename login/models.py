67bb5ddea39d885a2472b4b8aac06383a648a8c4
hkimia <kimiahamdieh@gmail.com>
2019-01-29 17:05:50 +0330

    models changed

--- login/models.py
+++ login/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class JobSeekerProfile(models.Model):
+    class Meta:
+        verbose_name = _('Profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # first_name = models.CharField(max_length=10, verbose_name='نام')
+    email = models.EmailField(name='email', primary_key=True)
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    tags = models.ManyToManyField(Tag, related_name='tags')
+    bio = models.TextField()
+    joined_date = models.DateField(auto_now_add=True, null=True, blank=True)
+    link = models.URLField()
+    cv = models.FileField(upload_to='files/cvs', null=True, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            JobSeekerProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(name='email', primary_key=True)
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    # TODO: not finished yet
+
+

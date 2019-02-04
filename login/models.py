2d5f39c2809c582b0a096d879196da6bc31c7988
aminrakhsha <a.rakhsha144@gmail.com>
2019-02-04 18:58:17 +0330

    ADD other advertisement category types

--- login/models.py
+++ login/models.py
@@ -73,7 +73,11 @@ class Choices:
     )
 
     JOBCATS = (
-        ('DEV', 'Developer'),
+        ('WEB_DEV', 'Web_Developer'),
+        ('MOBILE_DEV', 'Mobile_Developer'),
+        ('APP_DESIGN', 'Application_Design'),
+        ('PROJECT_MANAGEMENT', 'Project_Management'),
+        ('MARKETING', 'Marketing'),
     )
 
 
@@ -85,7 +89,7 @@ class Advertise(models.Model):
     title = models.CharField(max_length=100)
     deadline = models.DateField(null=True)
     type = models.CharField(max_length=1, choices=Choices.JOBTYPES)
-    category = models.CharField(max_length=3, choices=Choices.JOBCATS, blank=True)
+    category = models.CharField(max_length=20, choices=Choices.JOBCATS, blank=True)
     address = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
 
